c49310c144b607104dbec14b4337ff4536248ce8
ZinoTrust <zinotrust@gmail.com>
2023-07-10 18:35:36 +0100

    stripe webhook

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -156,7 +156,7 @@ const webhook = asyncHandler(async (req, res) => {
   eventType = event.type;
 
   // Handle the event
-  if (eventType === "checkout.session.completed") {
+  if (eventType === "checkout.session.async_payment_succeeded") {
     stripe.customers
       .retrieve(data.customer)
       .then(async (customer) => {
