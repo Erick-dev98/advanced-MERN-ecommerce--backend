080791b7cf970ac9d9da4e3e98e1398c40fd6fa4
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-03-20 19:34:22 +0300

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -99,7 +99,76 @@ const loginUser = asyncHandler (async (req, res) => {
     }
 });
 
+// Logout user
+const logout = asyncHandler (async (req, res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0),
+        // secure: true,
+        // samesite: none,
+    });
+    return res.status(200).json({message: "Successfully Logged Out"});
+});
+
+// Get User
+const getUser = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password")
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(400);
+        throw new Error("User Not Found");
+    }
+});
+
+// Get Login Status
+const getLoginStatus = asyncHandler (async (req, res) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json(false)
+    }
+    // Verify Token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (verified) {
+        res.json(true)
+    }
+    res.json(false)
+});
+
+// update user
+const updateUser = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        const { name, phone, address } = user;
+        user.name = req.body.name || name;
+        user.phone = req.body.phone || phone;
+        user.address = req.body.address || address;
+
+        const updateUser = await user.save();
+        res.status(200).json(updateUser);
+    } else {
+        es.status(404);
+        throw new Error("User not found");
+    }
+});
+
+// Update Photo
+const updatePhoto = asyncHandler (async (req, res) => {
+    const { photo } = req.body;
+    const user = await User.findById(req.user._id);
+    user.photo = photo;
+    const updatedUser = await user.save();
+    res.status(200).json(updatedUser);
+});
+
 module.exports = {
     registerUser,
     loginUser,
+    logout,
+    getUser,
+    getLoginStatus,
+    updateUser,
+    updatePhoto,
 };
